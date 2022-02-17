cc2a4d9bc26d0093a96b8e8ac519fd6181506b73
Michael Lowry <michael.lowry.20@cnu.edu>
2022-02-17 16:41:23 -0500

    did stuff; don't know what I'm doing

--- src/jsooooop/wikisooop.java
+++ src/jsooooop/wikisooop.java
@@ -1,5 +1,24 @@
 package jsooooop;
 
-public class wikisooop {
+import java.io.IOException;
 
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+
+public class wikisooop {
+	public static void main() {
+		String src = "https://en.wikipedia.org/wiki/Horse";
+		Document doc;
+		try {
+			doc = fetch(src);
+			System.out.println(doc);
+		} catch(IOException e) {
+			System.out.println(e);
+		}
+		
+	}
+	
+	public static Document fetch(String s) throws IOException {
+		return Jsoup.connect(s).get();
+	}
 }
